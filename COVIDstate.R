1b59f5dd0ef778ead97468fc7a525a338dcba0bc
Clairetea <claire.teague96@gmail.com>
2020-05-09 17:07:48 -0400

    experimenting code file; not necessarily reflective of final code

--- COVIDstate.R
+++ COVIDstate.R
@@ -13,20 +13,10 @@ statedata <- statedatao[statedatao$date >= (Sys.Date() - 14), ]
 states <- read.csv("states.csv")
 
 
-## Define a function that creates a dataframe for a given state, including the change in cases as a column
+## Define a function that creates a dataframe for a given state
 P <- function(state){
    s <- statedata[statedata$state == state, ]
    o <- s[order(s$date), ]
-   diffs <- NULL
-   
-   for (i in 2:nrow(o)){
-      val1 <- o[i, "cases"]
-      val2 <- o[i-1, "cases"]
-      diff <- val1 - val2
-      diffs <- c(diffs, diff)
-      
-   }
-   o$diff <- c(NA, diffs)
    o$state <- tolower(o$state)
    return(o)
 }
@@ -43,15 +33,15 @@ for (i in states$State){
 ## Reset W to Null
 W <- data.frame(
    region = tolower(states$State),
-   totaldiff = 1:51
+   totaldiff = 1:51,
+   isdecreasing = 1:51
 )
 
 ## Function that creates a dataframe with just the total change in cases
 Q <- function(state){
    df <- Z[[state]]
-   f <- sum(df$diff, na.rm = TRUE)
+   f <- df[nrow(df), "cases"] - df[1, "cases"]
    W[W$region == tolower(state), 2] <<- f
-   f
 }
 
 ## Execute Q across all states
@@ -59,16 +49,48 @@ for (i in states$State){
    Q(i)
 }
 
+## Add binary isdecreasing variable
+for (i in 1:51) {
+   val <- W[i, "totaldiff"]
+   if (val >= 0){
+      W[[i, "isdecreasing"]] <- 0
+   } else {
+      W[[i, "isdecreasing"]] <- 1
+   }
+}
+
+## Prepare map data
 states_map <- map_data("state")
 diff_map <- left_join(states_map, W, by = "region")
+
+## Create Cplot
 ggplot(diff_map, aes(long, lat, group = group))+
    theme_void() +
-   theme(legend.title = element_text(face="bold"))+
-   geom_polygon(aes(fill = totaldiff), color = "white")+
+   ggtitle("COVID-19 Cases: 14 Day Trajectory") +
+   theme(legend.title = element_text(face="bold"),
+         plot.title = element_text(face = "bold")) +
+   geom_polygon(aes(fill = totaldiff), color = "white") +
    scale_fill_gradient2(low = "green",
                         mid = "grey86",
                         midpoint = 0,
                         high = "red",
-                        name = "Total change in cases (in tens of thousands)",
-                        labels = paste(c(-1, 0, 1, 2, 3, 4)),
-                        breaks = c(-1, 0, 1, 2, 3, 4) * 10^5)
+                        name = "Total change in cases",
+                        labels = -10:10 * 10^4,
+                        breaks = -10:10 * 10^4)
+
+## Create bplot
+ggplot(diff_map, aes(long, lat, group = group))+
+   theme_void() +
+   ggtitle("COVID-19 Cases: Are Cases Decreasing?") +
+   theme(plot.title = element_text(face = "bold"),
+         legend.title = element_text(face="bold")) +
+   geom_polygon(aes(fill = factor(isdecreasing)), color = "white")+ 
+   scale_fill_manual(name = "Decreasing?",
+                     breaks = c(0, 1),
+                     labels = c("No", "Yes"),
+                     values = c("Red", "Green")
+      
+   )
+
+
+   
